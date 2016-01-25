93ce5610c01e2a57b7293e5d34ca62af92d96d9b
Anton Kiri <k3268@student.jamk.fi>
2016-01-25 20:14:49 +0200

    Latest version

--- Demo04/Tehtävä6/Sarjakuva.cs
+++ Demo04/Tehtävä6/Sarjakuva.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+* Copyright (C) Anton Kiri
+*
+*
+* Olio-ohjelmointi
+*
+*
+* 11.02.16
+* Anton Kiri
+*/
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
