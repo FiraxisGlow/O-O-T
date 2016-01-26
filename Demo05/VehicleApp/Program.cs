46e6c47df1b23558794ebf966b5f71985c6c2b77
Anton Kiri <K3268@student.jamk.fi>
2016-01-26 11:18:18 +0200

    latest version

---
+++ Demo05/VehicleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create tyres
+            Tyres tyre = new Tyres { Name = "Nokia", Model ="Hakkpaeliitta", Size = "205R16" };
+
+            //create a car
+            Vehicle car = new Vehicle { Name = "Honda", Model = "Accord" };
+            car.AddTyre(tyre); // car contains tyres...
+            car.AddTyre(tyre); // car contains tyres...
+            car.AddTyre(tyre); // car contains tyres...
+            car.AddTyre(tyre); // car contains tyres...
+
+            Console.WriteLine(car.ToString());
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Demo05/VehicleApp/Vehicle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleApp
+{
+    class Vehicle : Tyres
+    {
+        public string Name { get; set; }
+        public string Model { get; set; }
+
+        
+        public List<Tyres> Tyre { get; }
+        public Vehicle() //listan konstruktori
+        {
+            Tyre = new List<Tyres>();
+        }
+        
+        public void AddTyre(Tyres tyre)
+        {
+            Tyre.Add(tyre);
+        }
+
+        public override string ToString()
+        {
+            string sVehicle = "Vehicle Name: " + Name + " Model: " + Model + " Tyres:\n: ";
+            foreach(Tyres tyre in Tyre)
+            {
+                sVehicle += tyre.ToString() + "\n";
+            }
+            return sVehicle;
+        }
+
+
+    }
+}
